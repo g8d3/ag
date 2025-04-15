43eca02c09eceeff451498506b874e5865991323
Your Name <you@example.com>
2025-04-15 19:27:32 +0000

    Apr 15, 2025, 2:27 PM

--- old/cua/cua4.py
+++ old/cua/cua4.py
@@ -130,7 +130,7 @@ for model_id in models_to_test:
     )
     
     # goal = "Show me the contents of the current directory"
-    goal = "Show me the contents of the current directory"
+    goal = "read treechat.md and pass it again "
 
     # Measure time elapsed
     start_time = time.time()
